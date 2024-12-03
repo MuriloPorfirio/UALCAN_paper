3fc2ae3503370ecd7dea271d365eb7e373d8024d
Murilo Porfírio <166828742+MuriloPorfirio@users.noreply.github.com>
2024-12-03 20:42:29 -0300

    Update Dado_6.py

--- Dado_6.py
+++ Dado_6.py
@@ -30,4 +30,8 @@ plt.title("Scatter Plot: RAD54L vs ORC1L Expression Values", fontsize=14)
 plt.xlabel("RAD54L Expression Values (TPM)", fontsize=12)
 plt.ylabel("ORC1L Expression Values (TPM)", fontsize=12)
 plt.grid(True, linestyle="--", alpha=0.6)
+
+# Calcular a correlação de Pearson
+pearson_correlation = df_cleaned["RAD54L_expression_TPM"].corr(df_cleaned["ORC1L_expression_TPM"], method="pearson")
+print(f"Correlação de Pearson entre RAD54L e ORC1L: {pearson_correlation:.4f}")
 plt.show()
